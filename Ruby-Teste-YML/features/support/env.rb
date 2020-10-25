01cdb494b750aae1c7f3a94c84da04b62c577a3e
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-10-25 18:17:26 -0300

    Fixed chrome_headless method

--- Ruby-Teste-YML/features/support/env.rb
+++ Ruby-Teste-YML/features/support/env.rb
@@ -1,5 +1,6 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
+require_relative 'helper.rb'
 
 
 BROWSER = ENV['BROWSER']
@@ -7,16 +8,15 @@ AMBIENTE = ENV['AMBIENTE']
 
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
+World(Helper)
+
 #Configuração realizada para rodar em diversos browsers
 Capybara.register_driver :selenium do |app| #Sobrescrever o driver selenium
 if BROWSER.eql?('chrome')
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
 elsif BROWSER.eql?('chrome_headless')
-    Capybara::Selenium::Driver.new(app, :browser => :chrome, 
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-        'chromeOptions' => {'args' => ['--headless', 'disable-gpu']}
-    )
-    )
+    option = ::Selenium::WebDriver::Chrome::Options.new(args: ['--headless', '--disable-gpu', '--no-sandbox', '--window-size=1600,1024'])
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: option)
 elsif BROWSER.eql?('firefox')
     Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette=>true)
 elsif BROWSER.eql?('firefox_headless')
